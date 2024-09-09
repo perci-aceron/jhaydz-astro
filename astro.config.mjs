63bd7aaebf19b5b305e9f37a4045ead9f7f14a7b
Percival Aceron <perci.aceron@gmail.com>
2024-09-09 14:41:40 +0800

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  css: {
+    global: true,
+    files: ['src/styles/global.css'],
+  },
+
+  integrations: [preact()],
+});
